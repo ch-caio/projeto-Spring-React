e72c2f6707ee6b4f521553b8bc09152212e38c5f
Caio Pereira <ch.caio@hotmail.com>
2021-05-08 13:58:07 -0300

    group by search

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSumDTO.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SaleSumDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String SallerName;
+	private Double sum;
+	
+	public SaleSumDTO () {
+	}
+
+	public SaleSumDTO(Seller seller , Double sum) {
+		SallerName = seller.getName();
+		this.sum = sum;
+	}
+
+	public String getSallerName() {
+		return SallerName;
+	}
+
+	public void setSallerName(String sallerName) {
+		SallerName = sallerName;
+	}
+
+	public Double getSum() {
+		return sum;
+	}
+
+	public void setSum(Double sum) {
+		this.sum = sum;
+	}
+	
+}
